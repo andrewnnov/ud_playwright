d5f5952a1a8a4c568e54cb8cbe9b937713070729
andrewnnov <andrewnnov@yandex.ru>
2023-07-03 16:00:28 -0300

    k

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -22,8 +22,14 @@ export class LoginPage {
 
 
     // define page methods
-    async visit() {
-        await this.page.goto("http://zero.webappsecurity.com/");
+    async login(username: string, password: string) {
+        await this.usernameInput.type(username);
+        await this.passwordInput.type(password);
+        await this.submitButton.click();
+    }
+
+    async assertErrorMessage() {
+        await expect(this.errorMessage).toContainText("Login and/or password are wrong.");
     }
 
 
--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,36 +1,34 @@
 import {test, expect} from "@playwright/test"
 import { LoginPage } from "../../page-objects/LoginPage";
+import { HomePage } from "../../page-objects/HomePage";
 
 test.describe.parallel.only("Login / Logout Flow", () => {
 
     let loginPage: LoginPage;
+    let homePage: HomePage;
 
     //before hook
     test.beforeEach(async ({page}) => {
-        loginPage = new LoginPage(page);
-        //await page.goto('http://zero.webappsecurity.com/');
-        await loginPage.visit();
+        loginPage = new LoginPage(page);  
+        homePage = new HomePage(page);     
+        await homePage.visit();
     })
 
     //negative
 
-    test("Negative scenario for login", async ({page}) => {
-        await page.click("#signin_button")
-        await page.type("#user_login", "invalid username");
-        await page.type("#user_password", "invalid password")
-        await page.click("text=Sign in")
+    test("Negative scenario for login", async ({page}) => {        
+        await homePage.clickOnSignIn();      
+        await loginPage.login("invalidusername", "invalidpassword");
+        await loginPage.assertErrorMessage();
 
-        const errorMessage = await page.locator(".alert-error");
-        await expect(errorMessage).toContainText("Login and/or password are wrong.");  
+         
     })
 
     //positive
     test("Positive scenario for login + logout", async ({page}) => {
-        await page.click("#signin_button")
-        await page.type("#user_login", "username");
-        await page.type("#user_password", "password")
-        await page.click("text=Sign in")
-        //await page.pause();
+        await homePage.clickOnSignIn();        
+        await loginPage.login("username", "password");       
+
         const accountSummaryTab = await page.locator("#account_summary_tab");
         await expect(accountSummaryTab).toBeVisible();    
         
