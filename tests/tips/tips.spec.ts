8ef22ce6e8b1524cdb34500282e591b55dcb4fa3
andrewnnov <andrewnnov@yandex.ru>
2023-07-10 13:53:04 -0300

    fixme annotation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -3,14 +3,18 @@ import {test, expect} from '@playwright/test'
 test.describe.only("Tips and Trics Section", () => {
     test("TestInfo object", async ({page}, TestInfo) => {
         await page.goto("https://www.example.com");
-        console.log(TestInfo.title);
+        //console.log(TestInfo.title);
 
     })
 
     test("Test skip browser", async ({page, browserName}) => {
         test.skip(browserName === "chromium", "Feature nor ready in chrome browser")
         await page.goto("https://www.example.com");
+    })
 
+    test("Test Fixme annotation", async ({page, browserName}) => {
+        test.fixme(browserName === "chromium", "Test is not stable needs revision")
+        await page.goto("https://www.example.com");
     })
 })
 
